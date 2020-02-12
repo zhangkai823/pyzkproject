723c93b69d3f64289a32ceb9711aa394fa5a493f
zk <2642990218@qq.com>
2020-02-12 11:51:41 +0800

    there

---
+++ end/demos/bookdemo/booktest/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Book(models.Model):
+    """
+    book继承了Model类
+    """
+    title = models.CharField(max_length=20)
+    pub_date = models.DateField(default="1983-06-01")
+    price = models.FloatField(max_length=100,default=100)
+
+
+class Hero(models.Model):
+    """
+    hero继承了Model
+    """
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=7, choices=(('male', '男'), ('female', '女')), default='male')
+    content = models.CharField(max_length=100)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
