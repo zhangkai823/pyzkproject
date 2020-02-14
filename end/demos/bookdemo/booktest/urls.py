809161da6987f4baf681ed9a4feac454217a3a07
zk <2642990218@qq.com>
2020-02-14 17:55:52 +0800

    增加功能

--- end/demos/bookdemo/booktest/urls.py
+++ end/demos/bookdemo/booktest/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     # 以空开头 以空结尾  不需要打index 直接进入首页
     url(r'^$',views.index,name='index'),
     url(r'detail/(\d+)/',views.detail,name='detail'),
-    url(r'^about/$',views.about,name='about')
+    url(r'^about/$',views.about,name='about'),
+    url(r'^delete/(\d+)/$',views.detele,name='delete'),
+    url(r'^deletehero/(\d+)/$',views.deletehero,name='deletehero'),
+    url(r'^addhero/(\d+)/$', views.addhero,name='addhero'),
+    url(r'^edithero/(\d+)/$',views.edithero,name='edithero')
+
 ]
--- end/demos/bookdemo/booktest/views.py
+++ end/demos/bookdemo/booktest/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.template import loader
-from .models import Book
+from .models import Book,Hero
 # Create your views here.
 
 # 编写对应的视图函数
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 def index(request):
     # return HttpResponse("这里是首页")
@@ -23,7 +23,49 @@ def detail(request,bookid):
     # context = {"book":book}
     # result = template.render(context)
     # return HttpResponse(result)
-
     return render(request,"detail.html",{"book":book})
+
+def detele(request,bookid):
+    book = Book.objects.get(id=bookid)
+    book.delete()
+    return HttpResponseRedirect(redirect_to="/")
+
+def deletehero(request,heroid):
+    hero = Hero.objects.get(id=heroid)
+    bookid = hero.book.id
+    hero.delete()
+    url = reverse("booktest:detail",args=(bookid,))
+    return redirect(to=url)
+
+
+def edithero(request,heroid):
+    hero = Hero.objects.get(id=heroid)
+    if request.method == 'GET':
+        return render(request,'edithero.html',{'hero':hero})
+    elif request.method == 'POST':
+        hero.name = request.POST.get('heroname')
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("sex")
+        hero.save()
+        url = reverse("booktest:detail",args=(hero.book.id,))
+        return redirect(url)
+
+
+
+
+def addhero(request,bookid):
+    if request.method == 'GET':
+        return render(request,'addhero.html')
+    elif request.method == 'POST':
+        hero = Hero()
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("sex")
+        hero.book = Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse("booktest:detail",args=(bookid,))
+        return redirect(to=url)
+
+
 def about(request):
     return HttpResponse("这里是关于")
